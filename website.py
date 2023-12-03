c1de3e7d248d469a047d9477d82a4d39e99da7ee
gv21 <gaelle.verdon@epfl.ch>
2023-12-03 02:07:00 +0100

    update website avec infos demandées aux utilisateurs

--- website.py
+++ website.py
@@ -1,10 +1,39 @@
 import streamlit as st
 import datetime
+import helpers as hlp
+import api_requests as api
+import geocoder
+
+api.use_token()
 
 st.title('Your journey')
 
-origin = st.text_input("Where do you want to start?", "")
-destination = st.text_input("Where do you want to go?", "")
+current_pos=st.selectbox('Do you want to start from your current position?', ('Choose an option', 'Yes', 'No'))
+if (current_pos=='Yes'): 
+    #extract geolocalisation
+    g = (geocoder.ip('me')).latlng
+    origin = [g[1], g[0]]
+elif(current_pos=='No'):
+    origin_ = st.text_input(":blue[Please enter a stop place where you want to start]", "")
+    if (origin_!="") : 
+        #select from the multiple options
+        possible_origin=hlp.get_stations(origin_)
+        origin_name = st.selectbox(':blue[Please select the corresponding station:]', tuple(possible_origin['name']))
+        origin = possible_origin[possible_origin['name']==origin_name]['centroid'].values[0]
+
+current_pos_destintation=st.selectbox('Do you want to take your current position as destination?', ('Choose an option', 'Yes', 'No'))
+if (current_pos_destintation=='Yes'): 
+    #extract geolocalisation
+    g_ = (geocoder.ip('me')).latlng
+    destination = [g_[1], g_[0]]
+elif(current_pos_destintation=='No'):
+    destination_ = st.text_input(":blue[Where do you want to go?]", "")
+    if (destination_!="") : 
+        #select from the multiple options
+        possible_destination=hlp.get_stations(destination_)
+        destination_name = st.selectbox(':blue[Please select the corresponding station:]', tuple(possible_destination['name']))
+        destination = possible_destination[possible_destination['name']==destination_name]['centroid'].values[0]
+
 
 day = st.date_input('Departune day')
 t = st.time_input('Departune time', key=datetime.datetime.now())
@@ -28,9 +57,6 @@ data_user = {'origin':origin, 'destination':destination, 'departune_day':day, 'd
 
 
 st.markdown("""---""")
-if (origin==""and destination==""): st.write('Please provide details of your journey')
-elif (origin=="" and destination!=""): st.write('Please provide a starting point')
-elif (destination==""and origin!=""): st.write('Please provide a destination')
 
 #Run notre algo
 
